4626c63827007fe76e46c7fc0b6f9eac7ba80881
Denis Dikiy <denis.dikiy@gmail.com>
2025-01-24 01:20:13 +0200

    update locale file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Dashboard</title>
+    <title>@lang('Task Dashboard')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css'])
 
